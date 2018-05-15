770445edb61dbcc5d21eb1e91e6f5568866e3670
Robinson Greig <robinsongreig@gmail.com>
2018-05-15 18:06:59 -0400

    add support for halfbakedharvest and smittenkitchen

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,10 +3,43 @@ class Recipe < ApplicationRecord
 
   def get_recipe_details
     require 'open-uri'
-    doc = Nokogiri::HTML(open(self.link))
-    self.name = doc.css('h1.entry-title').text
+    @doc = Nokogiri::HTML(open(self.link, 'User-Agent' => 'firefox'))
+
+    case
+    when link.include?("minimalistbaker.com")
+      self.minimalist_baker
+    when link.include?("halfbakedharvest.com")
+      self.half_baked_harvest
+    when link.include?("smittenkitchen.com")
+      self.smitten_kitchen
+    end
+  end
+
+  def minimalist_baker
     self.ingredients = []
-    ingredients = doc.css('.ingredient')
+    self.name = @doc.css('h1.entry-title').text
+    self.image_url = @doc.css('.entry-content img').first.attr('src')
+    ingredients = @doc.css('.ingredient')
+    ingredients.each do |ingredient|
+      self.ingredients << ingredient.text
+    end
+  end
+
+  def half_baked_harvest
+    self.ingredients = []
+    self.name = @doc.css('h1.post-title').text
+    self.image_url = @doc.css('.post-content img').first.attr('src')
+    ingredients = @doc.css('.wprm-recipe-ingredient')
+    ingredients.each do |ingredient|
+      self.ingredients << ingredient.text
+    end
+  end
+
+  def smitten_kitchen
+    self.ingredients = []
+    self.name = @doc.css('h1.entry-title > a').text
+    self.image_url = @doc.css('.post-thumbnail-container > img').attr('src')
+    ingredients = @doc.css('.jetpack-recipe-ingredient')
     ingredients.each do |ingredient|
       self.ingredients << ingredient.text
     end
