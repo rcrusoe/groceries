a861cb8af1a10b5fbead2245a50d44e4d46f9a06
Robinson Greig <robinsongreig@gmail.com>
2018-07-03 09:21:52 -0400

    add browser caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,10 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, s-maxage=#{365.days.to_i}, maxage=#{180.days.to_i}",
+    'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
+  }
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
