9e72fccbefcd8cbde9aefdbfb508c2ee2ed67311
Robinson Greig <robinsongreig@gmail.com>
2018-07-21 15:55:31 -0400

    style button

--- public/js/button.js
+++ public/js/button.js
@@ -1,7 +1,17 @@
 (function() {
-  const link =  "http://www.recidex.com/import?recipe[link]=" + window.location.href
-  document.getElementById("recidex-button").setAttribute("href", link);
-  document.getElementById("recidex-button").addEventListener("click", function(e){
+  const link =  "http://www.recidex.com/import?recipe[link]=" + window.location.href;
+  const recidexButton = document.getElementById("recidex-button");
+  recidexButton.setAttribute("href", link);
+  recidexButton.innerHTML="Save on Recidex";
+  recidexButton.style["border"]="2px solid #454853"
+  recidexButton.style["border-radius"]="3px"
+  recidexButton.style["font-family"]="'Futura PT', sans-serif"
+  recidexButton.style["padding"]="10px"
+  recidexButton.style["white-space"]="nowrap"
+  recidexButton.style["text-decoration"]="none"
+  recidexButton.style["font-weight"]="bold"
+
+  recidexButton.addEventListener("click", function(e){
     e.preventDefault()
     let myWindow = window.open(link, "Save on Recidex", "width=550,height=530,top=100,left=200");
     return false;
